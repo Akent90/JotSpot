70964caf1d4c5f5ae5e6c0b3540d97087dd8fd0f
akent <alexanderkent099@gmail.com>
2024-04-09 12:27:10 -0400

    Correcting route path to icon

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,15 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
 let deferredPrompt;
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     deferredPrompt = event;
     butInstall.style.display = 'block';
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     butInstall.style.display = 'none';
     deferredPrompt.prompt();
@@ -18,7 +16,6 @@ butInstall.addEventListener('click', async () => {
     deferredPrompt = null;
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     deferredPrompt = null;
     console.log('PWA was installed', event);
